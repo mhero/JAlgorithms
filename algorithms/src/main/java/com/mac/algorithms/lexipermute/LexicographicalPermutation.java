1816e491e105aea1d55e511a120df604c6d48c8f
Marco <marco-aviles@hotmail.com>
2016-07-31 21:33:21 -0500

    Next lexicographical permutation
    Next lexicographical permutation

---
+++ algorithms/src/main/java/com/mac/algorithms/lexipermute/LexicographicalPermutation.java
@@ -0,0 +1,43 @@
+package com.mac.algorithms.lexipermute;
+
+public class LexicographicalPermutation {
+
+    private static boolean nextPermutation(char[] array) {
+        // Find non-increasing suffix
+        int i = array.length - 1;
+        while (i > 0 && array[i - 1] >= array[i]) {
+            i--;
+        }
+        if (i <= 0) {
+            return false;
+        }
+
+        // Find successor to pivot
+        int j = array.length - 1;
+        while (array[j] <= array[i - 1]) {
+            j--;
+        }
+        char temp = array[i - 1];
+        array[i - 1] = array[j];
+        array[j] = temp;
+
+        // Reverse suffix
+        j = array.length - 1;
+        while (i < j) {
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static String nextPermutation(String str) {
+        char[] toCharArray = str.toCharArray();
+        if(nextPermutation(toCharArray)){
+           return String.valueOf(toCharArray);
+        }
+        return null;
+    }
+}
---
+++ algorithms/src/main/java/com/mac/algorithms/lexipermute/Main.java
@@ -0,0 +1,22 @@
+package com.mac.algorithms.lexipermute;
+
+import static com.mac.algorithms.lexipermute.LexicographicalPermutation.nextPermutation;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner=new Scanner(System.in);
+        Integer lines=Integer.parseInt(scanner.nextLine());
+        
+        for(int i=0;i<lines;i++){
+            String word= nextPermutation(scanner.nextLine());
+            if(word!=null)
+                System.out.println(word);
+            else   
+                System.out.println("no answer");
+        }
+    }
+
+}
